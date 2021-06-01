4c8665261010d0ad84d10c6e9149af733f0a1367
slalom0914 <slalom0914@gmail.com>
2021-06-01 15:56:43 +0900

    의존성문제 제기

--- spring4_1_1/src/web/mvc/Board41Controller.java
+++ spring4_1_1/src/web/mvc/Board41Controller.java
@@ -1,11 +1,31 @@
 package web.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class Board41Controller extends MultiActionController {
+	Logger logger = Logger.getLogger(Board41Controller.class);
 	private Board41Logic boardLogic = null;
 	//setter메소드를 통하여 게으른 객체 주입
 	public void setBoardLogic(Board41Logic boardLogic) {
 		this.boardLogic = boardLogic;
 	}
+	//request로 유지
+	//메소드를 정의하는 것은 가능하다.
+	//파라미터가 없이도 괜찮은건가?
+	public ModelAndView getBoardList() {
+		logger.info("getBoardList 호출 성공");
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
+	//json으로 내보내준다. - @RestController:String, @Controller:void, ModelAndView, String
+	public void jsonGetBoardList(HttpServletRequest req, HttpServletResponse res) {
+		logger.info("jsonGetBoardList 호출 성공");
+		
+	}
+
 }
