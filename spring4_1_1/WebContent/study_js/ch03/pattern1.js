117e69b9c477e37ca02a7024337fad8ce60e9a46
slalom0914 <slalom0914@gmail.com>
2021-06-08 12:00:13 +0900

    함수정리

---
+++ spring4_1_1/WebContent/study_js/ch03/pattern1.js
@@ -0,0 +1,16 @@
+//제공 함수를 통한 패턴 분석1
+{
+  //console.log('test');
+  'use strict'
+  let addNum = 0;
+  let subNum = 100;
+
+  const auto_1 = setInterval(function(){
+    addNum++;
+    console.log("addNum: "+ addNum);
+  },3000);
+  const auto_2 = setInterval(function(){
+    subNum--;
+    console.log("subNum: "+ subNum);
+  },3000);
+}
---
+++ spring4_1_1/WebContent/study_js/ch03/pattern2.js
@@ -0,0 +1,11 @@
+//제공 함수를 통한 패턴 분석1
+{
+  //console.log('test');
+  'use strict'
+  let addNum = 0;
+  const auto = setTimeout(function(){
+    addNum++;
+    console.log(addNum);
+  },3000);
+
+}
